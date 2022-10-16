f770a55dd95b6dfd78f462f46f0c7686aa139191
Daivison Morais <daivison13pinheiro@gmail.com>
2022-10-16 19:07:18 -0300

    correção no database

--- src/conectionPG.js
+++ src/conectionPG.js
@@ -1,26 +1,13 @@
-/*import pkg from "pg";
+import pkg from "pg";
 
 const { Pool } = pkg;
 
- const databaseConfig = {
+const databaseConfig = {
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 };
 const connection = new Pool(databaseConfig);
-
-export default connection; */
-
-import pkg from "pg";
-
-const { Pool } = pkg;
-const connection = new Pool({
-  host: process.env.DATABASE_URL,
-  port: 5432,
-  user: "postgres",
-  password: "1441",
-  database: "shortly",
-});
 
 export default connection;
